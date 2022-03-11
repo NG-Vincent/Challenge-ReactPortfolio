2966376436f39640b416c81717883be46ad21344
Kelerik <vincent.ng39@gmail.com>
2022-03-10 19:38:51 -0500

    about component initial structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import Nav from "./components/Nav";
+import About from "./components/About";
 
 function App() {
    return (
       <div>
          <Nav />
+         <main>
+            <About />
+         </main>
       </div>
    );
 }
---
+++ src/components/About.js
@@ -0,0 +1,19 @@
+import React, { useEffect } from "react";
+
+function About(props) {
+   return (
+      <div>
+         <h3>About Me</h3>
+         <p>
+            Hey, I'm Vincent, and I love tech! Programming has been a big hobby
+            of mine since my teenage years, and I hope to put my talents to good
+            use. I have confidence in the entirety of full-stack development,
+            whether that be the front-end user experience, or the back-end
+            server optimization. Below, you will find my various projects,
+            including my schoolwork and even some of my hobby work.
+         </p>
+      </div>
+   );
+}
+
+export default About;

36f8eae42c4e60f8c2e602362fbad5576d956b90
Kelerik <vincent.ng39@gmail.com>
2022-03-10 19:33:42 -0500

    header component initial structure

---
+++ src/components/Nav.js
@@ -0,0 +1,19 @@
+import React, { useEffect } from "react";
+
+function Nav(props) {
+   return (
+      <header>
+         <h2>Vincent Nguyen</h2>
+         <nav>
+            <ul>
+               <li>About Me</li>
+               <li>Portfolio</li>
+               <li>Contact</li>
+               <li>Resume</li>
+            </ul>
+         </nav>
+      </header>
+   );
+}
+
+export default Nav;

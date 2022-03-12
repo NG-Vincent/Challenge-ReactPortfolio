2b93b3e6e36bab07e587566d13c4c9935c7bdf6a
Kelerik <vincent.ng39@gmail.com>
2022-03-11 19:49:02 -0500

    portfolio project array mapping

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,72 +1,74 @@
 function Portfolio(props) {
+   const projects = [
+      {
+         title: "Job Tracker",
+         img: "jobtracker",
+         alt: "Webpage of a job tracking app.",
+         github: "https://github.com/NG-Vincent/P2G2-JobTracker/",
+         deploy: "https://safe-cove-20593.herokuapp.com/",
+      },
+      {
+         title: "Lyrics Finder",
+         img: "lyricsfinder",
+         alt: "Webpage of a song and lyric search engine.",
+         github: "https://github.com/chamibia/lyrics-finder/",
+         deploy: "https://chamibia.github.io/lyrics-finder/",
+      },
+      {
+         title: "Run Buddy",
+         img: "runbuddy",
+         alt: "A runner tying their shoes and getting ready to go for a jog.",
+         github: "https://github.com/NG-Vincent/run-buddy/",
+         deploy: "https://ng-vincent.github.io/run-buddy/",
+      },
+      {
+         title: "Note Taker",
+         img: "notetaker",
+         alt: "Webpage of a note taking app.",
+         github: "https://github.com/NG-Vincent/Challenge-NoteTaker/",
+         deploy: "https://intense-springs-39510.herokuapp.com/",
+      },
+      {
+         title: "Tech Blog",
+         img: "techblog",
+         alt: "Webpage of a tech blog app.",
+         github: "https://github.com/NG-Vincent/Challenge-TechBlog/",
+         deploy: "https://sleepy-brook-70860.herokuapp.com//",
+      },
+      {
+         title: "Budget Tracker",
+         img: "budgettracker",
+         alt: "Webpage of a budget tracker app.",
+         github: "https://github.com/NG-Vincent/Challenge-BudgetTracker/",
+         deploy: "https://ancient-beyond-15125.herokuapp.com/",
+      },
+   ];
+
    return (
       <section id="projects">
          <h2>Projects</h2>
-         <a className="project" href="https://safe-cove-20593.herokuapp.com/">
-            <h3>Job Tracker</h3>
-            <img
-               className="placeholder-img"
-               src={require("../assets/img/jobtracker.jpg")}
-               alt="Webpage of a song and lyric search engine."
-            />
-         </a>
-
-         <a
-            className="project"
-            href="https://github.com/chamibia/lyrics-finder/"
-         >
-            <h3>Lyrics Finder</h3>
-            <img
-               className="placeholder-img"
-               src={require("../assets/img/artistlyrics.jpg")}
-               alt="Webpage of a song and lyric search engine."
-            />
-         </a>
-
-         <a className="project" href="https://ng-vincent.github.io/run-buddy/">
-            <h3>Run Buddy</h3>
-            <img
-               className="placeholder-img"
-               src={require("../assets/img/runbuddy.jpg")}
-               alt="A runner tying their shoes and getting ready to go for a jog."
-            />
-         </a>
-
-         <a
-            className="project"
-            href="https://intense-springs-39510.herokuapp.com/"
-         >
-            <h3>Note Taker</h3>
-            <img
-               className="placeholder-img"
-               src={require("../assets/img/notetaker.jpg")}
-               alt="Webpage for a note taking app."
-            />
-         </a>
-
-         <a
-            className="project"
-            href="https://sleepy-brook-70860.herokuapp.com/"
-         >
-            <h3>Tech Blog</h3>
-            <img
-               className="placeholder-img"
-               src={require("../assets/img/techblog.jpg")}
-               alt="Webpage for a tech blog app."
-            />
-         </a>
-
-         <a
-            className="project"
-            href="https://ancient-beyond-15125.herokuapp.com/"
-         >
-            <h3>Budget Tracker</h3>
-            <img
-               className="placeholder-img"
-               src={require("../assets/img/budgettracker.jpg")}
-               alt="Webpage for a budget tracker app."
-            />
-         </a>
+         {projects.map((info) => (
+            <div className="project" key={info.title}>
+               <h3>{info.title}</h3>
+               <img
+                  className="project-img"
+                  src={require(`../assets/img/${info.img}.jpg`)}
+                  alt={info.alt}
+               />
+               <ul>
+                  <li>
+                     <a href={info.github} target="_blank" rel="noreferrer">
+                        See on Github
+                     </a>
+                  </li>
+                  <li>
+                     <a href={info.deploy} target="_blank" rel="noreferrer">
+                        Go to App
+                     </a>
+                  </li>
+               </ul>
+            </div>
+         ))}
       </section>
    );
 }

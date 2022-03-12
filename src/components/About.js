d7be6722372a1f312cef10f6bd8ac0a264c60791
Kelerik <vincent.ng39@gmail.com>
2022-03-11 19:54:45 -0500

    add portrait image to "about" component

--- src/components/About.js
+++ src/components/About.js
@@ -2,6 +2,14 @@ function About(props) {
    return (
       <div>
          <h2>About Me</h2>
+
+         <div>
+            <img
+               className="about-img"
+               src={require("../assets/img/portrait.jpg")}
+               alt="My Portrait!"
+            />
+         </div>
          <p>
             Hey, I'm Vincent, and I love tech! Programming has been a big hobby
             of mine since my teenage years, and I hope to put my talents to good

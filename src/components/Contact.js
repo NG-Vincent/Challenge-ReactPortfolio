3bdf5d6e3ae274a0bf8d55b39daf6cbcb0cda92f
Kelerik <vincent.ng39@gmail.com>
2022-05-25 13:48:37 -0400

    remove face contact form, replace with links

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,47 +2,85 @@ import React, { useState } from "react";
 import { validateEmail } from "../utils/helpers";
 
 function Contact(props) {
-   const [formState, setFormState] = useState({
-      name: "",
-      email: "",
-      message: "",
-   });
+   // const [formState, setFormState] = useState({
+   //    name: "",
+   //    email: "",
+   //    message: "",
+   // });
 
-   const [errorMessage, setErrorMessage] = useState("");
-   const { name, email, message } = formState;
+   // const [errorMessage, setErrorMessage] = useState("");
+   // const { name, email, message } = formState;
 
-   const handleSubmit = (e) => {
-      e.preventDefault();
-      if (!errorMessage) {
-         console.log("Submit Form", formState);
-      }
-   };
+   // const handleSubmit = (e) => {
+   //    e.preventDefault();
+   //    if (!errorMessage) {
+   //       console.log("Submit Form", formState);
+   //    }
+   // };
 
-   const handleChange = (e) => {
-      if (e.target.name === "email") {
-         const isValid = validateEmail(e.target.value);
-         if (!isValid) {
-            setErrorMessage("Your email is invalid.");
-         } else {
-            setErrorMessage("");
-         }
-      } else {
-         if (!e.target.value.length) {
-            setErrorMessage(`${e.target.name} is required.`);
-         } else {
-            setErrorMessage("");
-         }
-      }
-      if (!errorMessage) {
-         setFormState({ ...formState, [e.target.name]: e.target.value });
-         console.log("Handle Form", formState);
-      }
-   };
+   // const handleChange = (e) => {
+   //    if (e.target.name === "email") {
+   //       const isValid = validateEmail(e.target.value);
+   //       if (!isValid) {
+   //          setErrorMessage("Your email is invalid.");
+   //       } else {
+   //          setErrorMessage("");
+   //       }
+   //    } else {
+   //       if (!e.target.value.length) {
+   //          setErrorMessage(`${e.target.name} is required.`);
+   //       } else {
+   //          setErrorMessage("");
+   //       }
+   //    }
+   //    if (!errorMessage) {
+   //       setFormState({ ...formState, [e.target.name]: e.target.value });
+   //       console.log("Handle Form", formState);
+   //    }
+   // };
 
    return (
-      <section>
+      <section className="contact">
          <h2 data-testid="h1tag">Contact me</h2>
-         <form id="contact-form" onSubmit={handleSubmit}>
+         <div>
+            <p>
+               Want to get in touch? Connect with me using any of the methods
+               listed below:
+            </p>
+            <ul>
+               <li>
+                  Email:{" "}
+                  <a
+                     href="mailto:vincent.ng39@gmail.com"
+                     target="_blank"
+                     rel="noreferrer"
+                  >
+                     vincent.ng39@gmail.com
+                  </a>
+               </li>
+               <li>
+                  GitHub:{" "}
+                  <a
+                     href="https://github.com/NG-Vincent"
+                     target="_blank"
+                     rel="noreferrer"
+                  >
+                     NG-Vincent
+                  </a>
+               </li>
+               <li>
+                  LinkedIn:{" "}
+                  <a
+                     href="https://www.linkedin.com/in/vincent-nguyen-4994bb221/"
+                     target="_blank"
+                     rel="noreferrer"
+                  >
+                     Vincent Nguyen
+                  </a>
+               </li>
+            </ul>
+         </div>
+         {/* <form id="contact-form" onSubmit={handleSubmit}>
             <div className="contact-section">
                <label htmlFor="name">Name:</label>
                <input
@@ -78,7 +116,7 @@ function Contact(props) {
             <button data-testid="button" type="submit">
                Submit
             </button>
-         </form>
+         </form> */}
       </section>
    );
 }
